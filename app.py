3059bcdeb87973e761bda07a65730f030c418fe2
Buch <buchita.gitchamnan@mydit.ie>
2021-03-07 13:16:02 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
@@ -8,19 +8,35 @@ mail = Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'bgitchamnan@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Yutaphong_123'
+app.config['MAIL_PASSWORD'] = '***'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route('/')
 def index():
-    # dict = {'phy': 50, 'che': 60, 'maths': 70}
-    # return render_template('main_page.html', result=dict)
-    msg = Message('Hello', sender='bgitchamnan@gmail.com', recipients=['buchita.gitchamnan@gmail.com'])
-    msg.body = "Hello Flask message sent from Flask-Mail"
-    mail.send(msg)
-    return "Sent"
+    return render_template('mail_form.html')
+
+
+
+
+@app.route('/mail', methods=['POST', 'GET'])
+def detail():
+   if request.method == 'POST':
+       # detail = request.form
+       heading = request.form.get("heading")
+       reciever = request.form.get("recipient")
+       massage = request.form.get("message")
+
+       msg = Message(heading, sender='buchita.gitchamnan@gmail.com', recipients=[reciever])
+       msg.body = massage
+
+       mail.send(msg)
+       return "Sent"
+
+
+       # return render_template("main_page.html", detail=detail)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
